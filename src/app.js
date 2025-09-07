d82c8dfe0f0eedf9e02f978d3e7dcede3434371c
Vivek sherwal <viveksherwal@gmail.com>
2025-09-08 01:00:45 +0530

    exploring get,put,delete

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const app = express();
 
+// app.use("/user",(req,res)=>{
+//     res.send("hhahahaahhhah");
+// });
 
-app.use("/hello",(req,res)=>{
-    res.send("hello hello");
-});
-app.use("/",(req,res)=>{
-    res.send("namastey bhes");
+//this will only handle get call to /user
+app.get("/user",(req,res)=>{
+    res.send({firstName : "bhes",lastname : "ki tang"});
 });
 
+app.post("/user",(req,res)=>{
+    //saving data to db
+    res.send("data is succesfully saved to the database");
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("delete bhes");
+});
+
+//thsi will match all the http method api calls to /test
 app.use("/test",(req,res)=>{
     res.send("hello from server ");
 });
