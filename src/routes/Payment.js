4053671b27c5c91bc457918230f7fd56c440060d
Vivek sherwal <viveksherwal@gmail.com>
2026-06-19 00:24:21 +0530

    premiumis

--- src/routes/Payment.js
+++ src/routes/Payment.js
@@ -92,5 +92,14 @@ paymentRouter.post("/payment/webhook", async (req, res) => {
   }
 });
 
+paymentRouter.get("/premium/verify",userauth,async(req,res)=>{
+  const user = req.user.toJSON();
+  if(user.isPremium){
+    return res.json({...user});
+  }
+  return res.json({...user});
+});
+
+
  
 module.exports = paymentRouter;
